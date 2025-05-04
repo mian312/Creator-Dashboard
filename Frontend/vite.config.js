7e9925ca16ff967c11ae773af2cd1916f36f7b88
mian312 <deymilan312@gmail.com>
2025-05-04 16:50:50 +0530

    trying to fix proxy on vercel

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,4 +1,4 @@
-// <reference types="vite/client" />
+/// <reference types="vite/client" />
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -13,6 +13,9 @@ export default ({ mode }) => {
       react(),
       tailwindcss()
     ],
+    define: {
+      __APP_ENV__: JSON.stringify(env.VITE_VERCEL_ENV),
+    },
     server: {
       proxy: {
         '/api': {
