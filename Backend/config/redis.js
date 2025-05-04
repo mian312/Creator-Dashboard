a128898bd19de3f04f8d27bdf884c4426bf931f2
mian312 <deymilan312@gmail.com>
2025-05-04 16:21:37 +0530

    trying to fix redis deployment

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const redisClient = createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
+  url: process.env.REDIS_URL,
 });
 
 redisClient.on('connect', () => {
