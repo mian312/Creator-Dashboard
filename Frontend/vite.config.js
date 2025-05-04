a22c63758558b77eeb078607f4c1c3d04e06333e
mian312 <deymilan312@gmail.com>
2025-05-04 16:59:23 +0530

    Fix Error: Direct api access

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,10 +1,11 @@
-/// <reference types="vite/client" />
+// <reference types="vite/client" />
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
 
 export default ({ mode }) => {
+  // Load environment variables based on the current mode
   const env = loadEnv(mode, process.cwd(), '');
 
   return defineConfig({
@@ -12,13 +13,10 @@ export default ({ mode }) => {
       react(),
       tailwindcss()
     ],
-    define: {
-      __APP_ENV__: JSON.stringify(env.VITE_VERCEL_ENV),
-    },
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_URL,
+          target: "https://creator-dashboard-6bdp.onrender.com", // Ensure this is set in your .env files
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/api/, ''),
